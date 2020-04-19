cc96ce6aa7450f52adeffdd0eab6758fca65245e
Phoebe Wong <phoebe.wong@berkeley.edu>
2020-04-19 17:27:52 -0400

    add data and init Shiny dashboard

--- Mass_COVID19/server.R
+++ Mass_COVID19/server.R
@@ -1,26 +1,40 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+library(shinydashboard)
+library(tidyverse)
+library(docxtractr)
+library(ggthemes)
 
-library(shiny)
+palettes <- ggthemes_data[["tableau"]][["color-palettes"]][["regular"]][["Tableau 10"]]
 
-# Define server logic required to draw a histogram
-shinyServer(function(input, output) {
+#### Read in data ####
+df <- read_docx("../data/covid-19-case-report-4-18-2020.docx")
+tbls <- docx_extract_all_tbls(df)
 
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
+function(input, output) {
+    #### Read in data ####
+    
+    output$county_bar <- renderPlot({
+        #### Preprocess ####
+        county_df <- tbls[[1]]
+        # county_names <- county_df$CATEGORY[2:16] # TODO: update this
+        
+        # Remove unrelated rows, before "Sex"
+        county_df <- county_df[2:(which(county_df$CATEGORY == "Sex")-1),]
+        
+        # Rename column
+        county_df <- county_df %>% 
+            rename(county = CATEGORY,
+                   num_case = NUMBER.OF.CONFIRMED.CASES) %>% 
+            mutate(num_case = as.numeric(num_case), # change to numeric
+                   county = fct_reorder(county, desc(num_case)))
+        
+        #### Plot ####
+        county_bar <- county_df %>% 
+            ggplot(aes(x = county, y = num_case)) +
+            geom_bar(stat = "identity", fill = palettes$value[1]) +
+            geom_text(aes(label = num_case), vjust = -0.3, size = 3) +
+            labs(x = "", y = "Number of Cases", title = "Number of COVID-19 cases in Massachusetts") +
+            theme_minimal() +
+            theme(axis.text.x = element_text(angle = 45))
+        county_bar
     })
-
-})
+}
--- Mass_COVID19/ui.R
+++ Mass_COVID19/ui.R
@@ -1,33 +1,17 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+library(shinydashboard)
 
-library(shiny)
-
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
+dashboardPage(
+    dashboardHeader(title = "Massachusetts COVID-19 Cases", 
+                    titleWidth = 350),
+    dashboardSidebar(disable=TRUE),
+    dashboardBody(
+        # Boxes need to be put in a row (or column)
+        fluidRow(
+            box(plotOutput("county_bar"), width = 12)#,
+            # box(
+            #     title = "Controls",
+            #     sliderInput("slider", "Number of observations:", 1, 100, 50)
+            # )
         )
     )
-))
+)
